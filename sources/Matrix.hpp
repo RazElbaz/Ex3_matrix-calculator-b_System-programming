4c928e4afcfe02b772594eb4c2be33732a6b80fd
Raz Elbaz <93310416+RazElbaz@users.noreply.github.com>
2022-04-13 10:33:23 +0300

    Add files via upload

--- sources/Matrix.hpp
+++ sources/Matrix.hpp
@@ -23,20 +23,27 @@ namespace zich{
             this->Columns = Columns;
             this->Values=MatrixVector;
         }
+
+        // Adding operators
         Matrix operator+(Matrix const &other);
         Matrix& operator+=(const Matrix &other);
         friend Matrix operator+(Matrix &other);
 
+        // Subtraction operator
         friend Matrix operator-(const Matrix &matrix1, const Matrix &matrix2);
         Matrix& operator-=(const Matrix &other);
         friend Matrix operator-(Matrix &other);
         Matrix operator-(const Matrix &other);
+
+        // Comparison operator
         bool operator>( Matrix &other);
         bool operator>=( Matrix &other);
         bool operator<( Matrix &other);
         bool operator<=( Matrix &other);
         bool operator==(const Matrix &other) const;
         bool operator!=(const Matrix &other) ;
+
+        // Increasement operator
         //++i
         Matrix& operator++();
         //i++
@@ -46,12 +53,16 @@ namespace zich{
         //--i
         Matrix& operator--();
 
+        // Scalar doubling
         friend Matrix operator*(const double scalar, Matrix &other);
         friend Matrix operator*(Matrix &other, const double scalar);
         Matrix& operator*=(const double scalar);
 
+        // Multiplication of two matrices
         Matrix operator*(const Matrix &other);
         Matrix& operator*=(const Matrix &other);
+
+        // friend global IO operators
         vector<string> Split_by_character(string will_split, char character);
         friend ostream& operator<< (ostream& output, const Matrix &matrix);
         friend istream& operator>> (istream& input, Matrix &matrix);
